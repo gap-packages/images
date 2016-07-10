2e0e6d4fe42ef52011677b870e83db690faa393a
Chris Jefferson <caj21@st-andrews.ac.uk>
2016-07-10 09:30:46 -0400

    Add initial documentation

--- gap/smallestImage.gd
+++ gap/smallestImage.gd
@@ -25,14 +25,54 @@ DeclareAttribute( "rowcolsquareGroup2", IsPermGroup );
 #F  MinimialImage
 ##
 
+#############################################################################
+##  <#GAPDoc Label="MinimalImage">
+##  <ManSection>
+##  <Func Name="MinimalImage" Arg="G[, pnt][, act][, Config]"/>
+##  <Func Name="IsMinimalImage" Arg="G[, pnt][, act][, Config]"/>
+##  <Func Name="MinimalImagePerm" Arg="G, [, pnt][, act][, Config]"/>
+##  <Description>
+##  <Ref Func="MinimalImage"/> returns the minimal image of <A>pnt</A> under
+##  the group <A>G</A>. <Ref Func="IsMinimalImage"/> returns a boolean which
+##  is <K>true</K> if <Ref Func="MinimalImage"/> would return <A>pnt</A> (so
+##  the value is it's own minimal image).
+##  <P/>
+##  <Ref Func="MinimalImagePerm"/> returns the permutation which maps
+##  <A>pnt</A> to its minimal image.
+##  <P/>
+##  The option <A>Config</A> defines a number of advanced configuration
+##  options, which are described in 'AdvancedConfig'.
+##  </Description>
+##  </ManSection>
+##  <#/GAPDoc>
 DeclareGlobalFunction("MinimalImage");
 DeclareGlobalFunction("IsMinimalImage");
 DeclareGlobalFunction("MinimalImagePerm");
 
 #############################################################################
-##
-#F  CanonicalImage
-##
+##  <#GAPDoc Label="CanonicalImage">
+##  <ManSection>
+##  <Func Name="CanonicalImage" Arg="G[, pnt][, act][, Config]"/>
+##  <Func Name="IsCanonicalImage" Arg="G[, pnt][, act][, Config]"/>
+##  <Func Name="CanonicalImagePerm" Arg="G, [, pnt][, act][, Config]"/>
+
+##  <Description>
+##  <Ref Func="CanonicalImage"/> returns a canonical image of <A>pnt</A> under
+##  the group <A>G</A>. <Ref Func="IsCanonicalImage"/> returns a boolean which
+##  is <K>true</K> if <Ref Func="CanonicalImage"/> would return <A>pnt</A> (so
+##  the value is it's own minimal image).
+##  <P/>
+##  <Ref Func="CanonicalImagePerm"/> returns the permutation which maps
+##  <A>pnt</A> to its minimal image.
+##  <P/>
+##  By default, these functions use the fasted known algorithm for calculating
+##  canonical images, which may vary with each version of this package.
+##  The option <A>Config</A> defines a number of advanced configuration
+##  options, which are described in 'AdvancedConfig'. These include the ability
+##  to choose the canonicalising algorithm used.
+##  </Description>
+##  </ManSection>
+##  <#/GAPDoc>
 DeclareGlobalFunction("CanonicalImage");
 DeclareGlobalFunction("IsCanonicalImage");
 DeclareGlobalFunction("CanonicalImagePerm");
